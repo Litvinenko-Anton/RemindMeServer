ff2e85e915f619c646f646e6c12d12c5f085eb69
Anton <380685545353@ya.ru>
2016-02-15 23:02:04 +0200

    Config base project. Create message "Hello World controller" on TomCat.
    http://localhost:8080/remind/get    displays    "My reminder".

---
+++ src/main/java/com/i7/remindme/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.i7.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.i7.remindme.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
